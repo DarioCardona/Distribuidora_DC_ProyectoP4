848205e080478911b6e6379c0cf70f743cd0b1ff
davcas24 <davidacastro.24@gmail.com>
2015-12-01 13:38:58 -0600

    Start

--- server/controllers/usuarioController.js
+++ server/controllers/usuarioController.js
@@ -1,5 +1,6 @@
 var usuario = require('../schemas/usuario');
-
+var SHA3 = require("crypto-js/sha3");
+var boom = require("Boom");
 exports.getusuario = {
   handler: function(request, reply){
     var usuario = usuario.find({});
@@ -8,23 +9,28 @@ exports.getusuario = {
 }
 
 exports.createusuario = {
+  auth: {
+      mode:'require',
+      strategy:'session'
+      scope: ["admin"]
+    },
   handler: function(request, reply){
-
     var newusuario = new usuario({
-
-      tipo : request.payload.user_user,
+      scope : request.payload.user_user,
       nombre : request.payload.user_name,
-      password: request.payload.user_password,
+      password: SHA3(request.payload.user_password),
       direccion : request.payload.user_dire,
       celular : request.payload.user_cel,
       correo: request.payload.user_mail,
       tel_fijo: request.payload.user_fijo,
       tabla : request.payload.user_tabla,
       zona : request.payload.user_zona
-
     });
     newusuario.save();
     console.log('usuario saved');
+    if(err){
+      return reply(boom.notAcceptable("BOOM" + err));
+    }
     return reply('ok');
   }
 }
