e2d780c0df8e1ff8f66158f6251e7609a3ffa7cf
davcas24 <davidacastro.24@gmail.com>
2015-12-01 15:12:07 -0600

    Saving

--- client/app/scripts/controllers/loginController.js
+++ client/app/scripts/controllers/loginController.js
@@ -2,9 +2,32 @@ angular.module('AngularScaffold.Controllers')
   .controller('loginController', ['$state','$scope', 'loginService', 'indexService', function ($state,$scope, loginService, indexService) {
 
   	indexService.setTitle("Distribuidora DC");
-
-      $scope.goadmin = function(){
+      /*$scope.goadmin = function(){
         $state.go('admin');
-        }
+      }*/
+      $scope.user = {};
+      $scope.$sessionStorage = $sessionStorage;
+      
+      $scope.logout = function(){
+        authService.Logout().then(function(response){
+          alert('logged out correctly');
+          $sessionStorage.$reset();
+        }).catch(function(err){
+          alert(err.data.error + " " + err.data.message);
+        })
+      }
+
+      $scope.login = function(user){
+        authService.Login(user).then(function(response){
+          $sessionStorage.currentUser = response.data;
+          $scope.user = {};
+        }).catch(function(err){
+          alert(err.data.error + " " + err.data.message);
+        });
+      }
+
 
   }]);
+
+  angular.module('AngularScaffold.Controllers')
+    .controller('NavbarController', ['AuthService', '$scope', '$rootScope', '$sessionStorage',  function (authService, $scope, $rootScope, $sessionStorage) {
--- server/controllers/usuarioController.js
+++ server/controllers/usuarioController.js
@@ -11,25 +11,25 @@ exports.getusuario = {
 exports.createusuario = {
   auth: {
       mode:'require',
-      strategy:'session'
-      scope: ["admin"]
+      strategy:'session',
+      scope: ['admin']
     },
   handler: function(request, reply){
     var newusuario = new usuario({
       scope : request.payload.user_user,
       nombre : request.payload.user_name,
-      password: SHA3(request.payload.user_password),
+      password : SHA3(request.payload.user_password),
       direccion : request.payload.user_dire,
       celular : request.payload.user_cel,
-      correo: request.payload.user_mail,
-      tel_fijo: request.payload.user_fijo,
+      correo : request.payload.user_mail,
+      tel_fijo : request.payload.user_fijo,
       tabla : request.payload.user_tabla,
       zona : request.payload.user_zona
     });
     newusuario.save();
     console.log('usuario saved');
     if(err){
-      return reply(boom.notAcceptable("BOOM" + err));
+      return reply(boom.notAcceptable("Error" + err));
     }
     return reply('ok');
   }
