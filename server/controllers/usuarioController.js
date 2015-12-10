52090c11ff040a0199da4acc740298f7bf52d3e4
Dario Cardona <dario.cardona@unitec.edu>
2015-12-09 18:52:55 -0600

    ahi va amigos

--- server/controllers/usuarioController.js
+++ server/controllers/usuarioController.js
@@ -1,30 +1,40 @@
 var usuario = require('../schemas/usuario');
+var SHA3 = require("crypto-js/sha3");
+var boom = require("Boom");
 
 exports.getusuario = {
   handler: function(request, reply){
-    var usuario = usuario.find({});
-    reply(usuario);
+    var usuarios = usuario.find({});
+    reply(usuarios);
   }
 }
 
 exports.createusuario = {
+  /*auth: {
+      mode:'required',
+      strategy:'session',
+      //scope: ['admin']
+    },*/
   handler: function(request, reply){
-
+    console.log(request.payload+"llego al backend");
     var newusuario = new usuario({
-
-      tipo : request.payload.user_user,
-      nombre : request.payload.user_name,
-      password: request.payload.user_password,
-      direccion : request.payload.user_dire,
-      celular : request.payload.user_cel,
-      correo: request.payload.user_mail,
-      tel_fijo: request.payload.user_fijo,
-      tabla : request.payload.user_tabla,
-      zona : request.payload.user_zona
-
+      scope : request.payload.scope,
+      nombre : request.payload.nombre,
+      password: SHA3(request.payload.password),
+      direccion : request.payload.direccion,
+      celular : request.payload.celular,
+      correo: request.payload.correo,
+      tel_fijo: request.payload.tel_fijo,
+      tabla : request.payload.tabla,
+      zona : request.payload.zona
     });
-    newusuario.save();
-    console.log('usuario saved');
-    return reply('ok');
+    newusuario.save(function(err){
+      if(err){
+        return reply(boom.notAcceptable("Error" + err));
+      }
+      console.log('usuario saved');
+      return reply('ok');
+    });
+
   }
-}
+};
